d490f7d749d5e25c287148f4a69fe059af3acc96
Zippy6666 <simperzippy@gmail.com>
2024-07-15 20:24:07 +0200

    better

--- lua/weapons/weapon_zippy_dummy.lua
+++ lua/weapons/weapon_zippy_dummy.lua
@@ -1,20 +1,11 @@
 AddCSLuaFile()
 
---[[
-======================================================================================================================================================
-                                           BASICS
-======================================================================================================================================================
---]]
-
-
--- The weapon's base script, relative to lua/weapons.
-SWEP.Base = "weapon_zwb_base"
-
-
+SWEP.Base = "weapon_zwb_base" -- The weapon's base script, relative to lua/weapons.
 SWEP.Spawnable = true -- Whether or not this weapon can be obtained through the spawn menu.
 SWEP.AdminOnly = false -- If spawnable, this variable determines whether only administrators can use the button in the spawn menu.
-SWEP.PrintName = "Dummy Weapon" -- The name of the SWEP displayed in the spawn menu.
+SWEP.PrintName = "Your Weapon" -- The name of the SWEP displayed in the spawn menu.
 SWEP.Author = "You" -- The SWEP's author.
 SWEP.Category = "Your Weapons" -- The spawn menu category that this weapon resides in.
+
 SWEP.Purpose = "Kill everyone."
 SWEP.Instructions = "Shoot 'em up."
