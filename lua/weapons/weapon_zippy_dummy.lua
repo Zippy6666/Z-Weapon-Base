c51a08c4a024358f81250378b8fd4d7a7184feee
Simon R00s <144894074+Zippy6666@users.noreply.github.com>
2024-07-15 21:13:41 +0200

    Update weapon_zippy_dummy.lua

--- lua/weapons/weapon_zippy_dummy.lua
+++ lua/weapons/weapon_zippy_dummy.lua
@@ -1,5 +1,7 @@
 AddCSLuaFile()
 
+SWEP.IsZWBWeapon = true -- Very important, do not remove!
+
 SWEP.Base = "weapon_zwb_base" -- The weapon's base script, relative to lua/weapons.
 SWEP.Spawnable = true -- Whether or not this weapon can be obtained through the spawn menu.
 SWEP.AdminOnly = false -- If spawnable, this variable determines whether only administrators can use the button in the spawn menu.
